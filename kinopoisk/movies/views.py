4e84ff55a6ba10f1867d75a4e366e9634d09b9ba
Vladimir Strelec <86953981+Vladimir-Strelec@users.noreply.github.com>
2022-11-11 20:35:11 +0300

    Сreate base and include file and logic

--- kinopoisk/movies/views.py
+++ kinopoisk/movies/views.py
@@ -13,7 +13,7 @@ class MoviesView(ListView):
 
 class MovieDetailView(DetailView):
     model = Movie
-    template_name = 'movies/moviesingle.html'
+    template_name = "movies/moviesingle.html"
 
     # def get(self, request, **kwargs):
     #     movie = Movie.objects.get(pk=self.kwargs['pk'])
@@ -22,3 +22,8 @@ class MovieDetailView(DetailView):
     #     rating_star = RatingStar.objects.filter(rating__movie_id=self.kwargs['pk'])
     #     return render(request, "movies/moviesingle.html", {'movie': movie})
 
+
+class SideBarView(ListView):
+    model = Movie
+    queryset = Movie.objects.all()[:5]
+    template_name = "include/side-bar.html"
